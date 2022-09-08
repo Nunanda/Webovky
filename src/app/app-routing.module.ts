8c16872dfc93dc498a6a5df81325aacd6bda7337
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2022-09-08 19:50:24 +0200

    vyukovymod+navody

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { VyukovymodComponent } from './pages/vyukovymod/vyukovymod.component';
+import { SlovnikComponent } from './pages/slovnik/slovnik.component';
 import { PomuckyComponent } from './pages/pomucky/pomucky.component';
+import { NavodyComponent } from './pages/navody/navody.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'vyukovymod', component: VyukovymodComponent },
+  { path: 'slovnik', component: SlovnikComponent },
+  { path: 'pomucky', component: PomuckyComponent },
+  { path: 'navody', component: NavodyComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BleComponent } from './pages/ble/ble.component';
 import { HomeComponent } from './pages/home/home.component';
 import { VyukovymodComponent } from './pages/vyukovymod/vyukovymod.component';
 import { SlovnikComponent } from './pages/slovnik/slovnik.component';
-import { PomuckyComponent } from './pomucky/pomucky.component';
-import { NavodyComponent } from './navody/navody.component';
+import { PomuckyComponent } from './pages/pomucky/pomucky.component';
+import { NavodyComponent } from './pages/navody/navody.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BleComponent,
     HomeComponent,
     VyukovymodComponent,
     SlovnikComponent,

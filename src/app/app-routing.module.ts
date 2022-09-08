9fd5230361cdeaeca817894598ef34dc8c10f280
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2022-09-08 12:02:13 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
-import {  PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{
-  path: 'home',
-  loadChildren: () => import('./pages/home/home.component').then( m => m.HomeComponent)
-},
-{
-  path: '',
-  redirectTo: 'home',
-  pathMatch: 'full'
-},
-/*{
-  path: 'vyukovymod',
-  loadChildren: () => import('./pages/vyukovymod/vyukovymod.component').then( m => m.VyukovymodComponent)
-}*/
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
-import { HomeComponent } from './pages/home/home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {

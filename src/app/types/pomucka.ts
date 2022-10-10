1c307b09bcef304a189ae579cd13788adc5802c9
Aneta Wernerová <aneta.werner@seznam.cz>
2022-10-10 17:59:01 +0200

    uprava-type

--- src/app/types/pomucka.ts
+++ src/app/types/pomucka.ts
@@ -4,8 +4,7 @@ export type Pomucka =  {
     popis: string,
     popis1: string,
     popis2: string,
-    fotka: string,
-    fotka1: string,
-    fotka2: string,
+    okno: boolean,
+    okno2: boolean,
     videoURL: string,
   }

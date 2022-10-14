ee34135adf677f5d2058dfd70fa109b1655c93ba
Václav Gryc <Vaclav.Gryc@ibm.com>
2022-10-14 12:26:01 +0200

    fix

--- src/app/types/navody.ts
+++ src/app/types/navody.ts
@@ -2,5 +2,18 @@ export type Navod =  {
   nazev: string,
   zkratky: string,
   pomucky: string,
-  popisy: any
+  popisy: Array<Popis>,
+  obtiznost: Obtiznost
+}
+
+export type Popis = {
+  obrazek: boolean,
+  nazevCasti: string,
+  popis: string
+}
+
+export enum Obtiznost {
+  tezky='tezky',
+  stredni='stredni',
+  lehky='lehky'
 }

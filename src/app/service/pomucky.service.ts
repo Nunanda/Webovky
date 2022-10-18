d1de73ca120b7ff1b8a249f523b2abf682dd6112
Aneta Wernerová <aneta.werner@seznam.cz>
2022-10-18 19:08:19 +0200

    uprava

--- src/app/service/pomucky.service.ts
+++ src/app/service/pomucky.service.ts
@@ -7,8 +7,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Existuje široká škála velikostí.", 
     popis1: "Háčky se dělají z různých materiálů.", 
     popis2: "Mohou být zdobeny rukojeťmi, ergonomickými úchyty a podobně.", 
+    obrazek: false,
+    obrazek1: true,
+    obrazek2: false,
+    okno1:true,
     okno2: true,
-    okno:true,
     videoURL: "" },
 
     { nazev: "prize", 
@@ -16,8 +19,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Dnes můžeme najít obrovské množství značek a materiálů (akrylové, bavlněné, směsové).", 
     popis1: "Na přebalu klubíčka bývá napsáno, kolik metrů obsahuje a na jak velký háček (popř. jehlice) je dělaná. ", 
     popis2: "Samozřejmě můžete využít více velikostí na jeden typ příze (např.: Dolphin Baby se dá háčkovat s háčkem velikosti 4, 4.5 nebo 5 mm), ovlivňuje se tím však velikost oček.", 
+    obrazek: true,
+    obrazek1: false,
+    obrazek2: false,
+    okno1:true,
     okno2: true,
-    okno:true,
     videoURL: "" },
 
     { nazev: "znackovac", 
@@ -25,8 +31,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Slouží nám k označení počátku řady, ale dá se použít i jiný kousek příze.", 
     popis1: "Značkovač je příjemná pomůcka, která se na trhu objevuje ve více variantách. Spirála je vhodnější pro silnější příze", 
     popis2: "Tento má výhodu v tom, že se dá zavřít a je vhodný pro všechny typy příze.", 
+    obrazek: false,
+    obrazek1: true,
+    obrazek2: true,
+    okno1:true,
     okno2: true,
-    okno:true,
     videoURL: "" },
 
     { nazev: "jehla", 
@@ -34,8 +43,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Na přišívání jednotlivých dílů, zapošívání nitě. Upřednostňované plastové jehly se více hodí, pokud děláte se silnějším typem příze.", 
     popis1: "", 
     popis2: "", 
+    obrazek: true,
+    obrazek1: false,
+    obrazek2: false,
+    okno1:false,
     okno2: false,
-    okno:false,
     videoURL: "" },
 
     { nazev: "nuzky", 
@@ -43,8 +55,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Nůžky asi už někdy používal každý z nás a u háčkování to není nějak zvlášť jiné.", 
     popis1: "Klasické nůžky na papír nejsou na některé příze vhodné, protože potom ji přestříháváme na několikrát a akorát se nám konec třepí.", 
     popis2: "Můžete si pořídit nůžky na látku, nebo různé speciálně zdobené, tvarované.", 
+    obrazek: false,
+    obrazek1: false,
+    obrazek2: false,
+    okno1:true,
     okno2: true,
-    okno:true,
     videoURL: "" },
 
     { nazev: "bezpecnostni-oci-a-nos", 
@@ -52,8 +67,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Dnes se dá pořídit spousta tvarů, velikostí a barev očí a nosů s bezpečnostní pojistkou.", 
     popis1: "Uzavírají se jednoduše. Na závit se postupně nasouvá pojistka, která jde zaklapnout tolikrát, kolik má očko závitů.", 
     popis2: "Čím pevněji oko uzavřeme tím hezčí, nebo ošklivější efekt můžeme vytvořit. Je to kus od kusu, výrobek od výrobku.", 
+    obrazek: true,
+    obrazek1: true,
+    obrazek2: true,
+    okno1:true,
     okno2: true,
-    okno:true,
     videoURL: "" },
 
     { nazev: "chrastitko", 
@@ -61,8 +79,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Pokud chcete svůj výrobek nějak ozvláštnit pro dítě, nebo třeba domácího mazlíčka, můžete dovnitř přidat chrastítko.", 
     popis1: "Ty se dnes vyrábí v různých velikostech, tvarech a každé chrastí jinou intenzitou.", 
     popis2: "", 
+    obrazek: true,
+    obrazek1: true,
+    obrazek2: true,
+    okno1:true,
     okno2: false,
-    okno:true,
     videoURL: "" },
 
     { nazev: "pocitadlo", 
@@ -70,8 +91,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Existuje počítadlo manuální a digitální. Obě mají stejný účel. Počítat řady, co jsme dosud vytvořili.", 
     popis1: "Manuální může vypadat jako válec a každá jeho strana se dá otáčet. Měníme tak číslovky od 0 do 9. Některá se dají připojit na háček, jiná musíte stále brát a pokládat bokem.", 
     popis2: "Mnohem pohodlnější je z tohoto důvodu počítadlo digitální, které se dá připevnit na palec a pouhým mačkáním tlačítek buď posouváte číslo o 1, nebo celý proces resetujete. Existují i taková počítadla, která umí displej rozsvítit.", 
+    obrazek: false,
+    obrazek1: true,
+    obrazek2: true,
+    okno1:true,
     okno2: true,
-    okno:true,
     videoURL: "" },
 
     { nazev: "navijec-prize", 
@@ -79,8 +103,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Když se vám klubíčko zasukuje tak, že ho musíte celé rozplést, nebo se vám rozkutálí, tak je navíječ pomocník pro vás. Jedná se o nástroj, který dělá z kusu provázku opět klubíčko. Jednoduše upevníte jeden konec na horní otáčecí válec, přízi napnete a pak už jen točíte kličkou. Navíječ vytvoří klubíčko.", 
     popis1: "", 
     popis2: "", 
+    obrazek: true,
+    obrazek1: true,
+    obrazek2: true,
+    okno1:false,
     okno2: false,
-    okno: false,
     videoURL: "" },
 
     { nazev: "vypln", 
@@ -88,8 +115,11 @@ const polePomucek: Array<Pomucka>= [
     popis: "Pokud vytváříte něco prostorového (zvířátka, hračky, atd...), tak se vám bude hodit nějaká výplň. Tu může tvořit vata, nebo duté vlákno. Po delší době háčkování můžete použít i zbytky přízí z minulého tvoření. Je to taková recyklace, kterou já uplatňuji třeba u jehelníčků.", 
     popis1: "", 
     popis2: "", 
+    obrazek: false,
+    obrazek1: true,
+    obrazek2: true,
+    okno1:false,
     okno2: false,
-    okno: false,
     videoURL: "" }]
 
     @Injectable({
@@ -112,6 +142,10 @@ const polePomucek: Array<Pomucka>= [
     
       public getPomuckaByName(name: string) {
         return this.mapaPomucek.get(name);
+      }
+
+      public getVsechnyPomucky(): Array<Pomucka> {
+        return polePomucek;
       }
     
     }
--- src/app/types/pomucka.ts
+++ src/app/types/pomucka.ts
@@ -4,7 +4,10 @@ export type Pomucka =  {
     popis: string,
     popis1: string,
     popis2: string,
-    okno: boolean,
+    okno1: boolean,
     okno2: boolean,
+    obrazek: boolean,
+    obrazek1: boolean,
+    obrazek2: boolean,
     videoURL: string,
   }

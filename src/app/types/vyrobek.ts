6ff62f0b41068848455422005befbba1004a9ee2
Szotkowski <mikiszotkowski@gmail.com>
2022-10-30 13:35:32 +0100

    pipeline yml

--- src/app/types/vyrobek.ts
+++ src/app/types/vyrobek.ts
@@ -2,5 +2,13 @@ export type Vyrobek = {
     nazev: string,
     popis: string,
     pocet: string,
-    kroky: any
+    kroky: Array<Info>
+}
+
+export type Info = {
+    cislo: string,
+    styl: string,
+    pouzijeme: string,
+    popis: string,
+    videoUrl: string
 }

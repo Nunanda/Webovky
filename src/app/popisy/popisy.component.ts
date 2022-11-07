4dda1abaf288065917d0fb2a3ce7934b72a3d97c
Szotkowski <mikiszotkowski@gmail.com>
2022-11-07 15:00:03 +0100

    next/previous buttons

--- src/app/popisy/popisy.component.ts
+++ src/app/popisy/popisy.component.ts
@@ -25,11 +25,21 @@ export class PopisyComponent implements OnInit {
   }
 
   public nextIndex() {
-    this.index++;
+    if (this.index++ > this.popis.length) {
+      this.index = 0;
+    }
+    else {
+      this.index++;
+    }
   }
 
   public previousIndex() {
-    this.index--;
+    if (this.index-- < 0) {
+      this.index = 0;
+    }
+    else {
+      this.index--;
+    }
   }
 
 }

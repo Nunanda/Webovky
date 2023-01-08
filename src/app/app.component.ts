756a2b0378d7a82d9110a3409ef3f43553415b86
Szotkowski <mikiszotkowski@gmail.com>
2023-01-08 20:07:20 +0100

    Předělání Tobiášova scriptu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,19 @@ import { Router } from '@angular/router';
 })
 
 export class AppComponent{
+
+  element!: HTMLElement | null;
+
   constructor(private router: Router) { }
   title = 'Webovky';
   ngOnInit() : void {
     this.router.navigate(['home']);
+    this.element = document.getElementById("myTopnav");
+  }
+
+  myFunction() {
+    if (this.element?.className === "topnav") {
+      this.element.className += " responsive";
+    }
   }
 }

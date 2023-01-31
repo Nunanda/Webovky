b4abeefb417fbb30c14d22d1e659fa480e1a5318
Szotkowski <mikiszotkowski@gmail.com>
2023-01-31 20:52:43 +0100

    responyivni menu fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,42 @@ import { Router } from '@angular/router';
 export class AppComponent {
 
   element: HTMLElement | null;
+  element1: HTMLElement | null;
 
   constructor(private router: Router) {
-    this.element = document.getElementById("myTopnav");
+    this.element = document.getElementById("mySidenav");
+    this.element1 = document.getElementById("main");
   }
   title = 'Webovky';
   ngOnInit(): void {
     this.router.navigate(['home']);
-    this.element = document.getElementById("myTopnav");
+    this.element = document.getElementById("mySidenav");
+    this.element1 = document.getElementById("main");
+    this.myFunction();
+  }
+
+  public openNav() {
+    this.element?.setAttribute("style", "width: 250px");
+    this.element1?.setAttribute("style", "marginLeft: 250px");
+  }
+
+  public closeNav() {
+    this.element?.setAttribute("style", "width: 0");
+    this.element1?.setAttribute("style", "marginLeft: 0");
   }
 
   public myFunction() {
-    if (this.element?.className === "topnav") {
-      this.element.className += " responsive";
-    }
-    else if (this.element?.className === "topnav responsive") {
-      this.element.className = "topnav";
-    }
+    /*var dropdown = document.getElementsByClassName("dropdown-btn");
+    for (let i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          dropdownContent.style.display = "block";
+        }
+      });
+    }*/
   }
 }

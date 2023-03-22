a70be03a84b5f415ea6e6c37844ab2930b348666
Aneta Wernerová <aneta.werner@seznam.cz>
2023-03-22 23:27:18 +0100

    registrace

--- src/app/pages/prihlaseni/registrace/registrace.component.ts
+++ src/app/pages/prihlaseni/registrace/registrace.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrace.component.css']
 })
 export class RegistraceComponent implements OnInit {
-
+  passwordToggleIcon = "visibility";
+  showPassword!: boolean;
+  passwordToggleIcon2 = "visibility";
+  showPassword2!: boolean;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public togglePassword() {
+    this.showPassword = !this.showPassword;
+    if (this.passwordToggleIcon == 'visibility') {
+      this.passwordToggleIcon = 'visibility_off';
+    }
+    else {
+      this.passwordToggleIcon = 'visibility';
+    }
+  }
+
+  public togglePassword2() {
+    this.showPassword2 = !this.showPassword2;
+    if (this.passwordToggleIcon2 == 'visibility') {
+      this.passwordToggleIcon2 = 'visibility_off';
+    }
+    else {
+      this.passwordToggleIcon2 = 'visibility';
+    }
+  }
 }

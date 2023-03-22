0af2acd8e5ae5748f313ebad8dae08f24ea14e6c
Aneta Wernerová <aneta.werner@seznam.cz>
2023-03-22 23:07:18 +0100

    ocicko

--- src/app/pages/prihlaseni/prihlaseni.component.ts
+++ src/app/pages/prihlaseni/prihlaseni.component.ts
@@ -6,38 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prihlaseni.component.css']
 })
 export class PrihlaseniComponent implements OnInit {
-  email!: string;
-  password!: string;
-  passwordToggleIcon = "eye";
+  passwordToggleIcon = "visibility";
   showPassword!: boolean;
-  usernameRegEx = new RegExp('^[a-zA-Z0-9_.-]{4,20}$');
-  emailRegEx = new RegExp('[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}');
-  passwordRegEx = new RegExp('^[a-zA-Z0-9?!.,_-]{8,20}$');
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  public usernameCheck(): boolean {
-    return this.usernameRegEx.test(this.email);
-  }
-
-  public emailCheck(): boolean {
-    return this.emailRegEx.test(this.email);
-  }
-
-  public passwordCheck(): boolean {
-    return this.passwordRegEx.test(this.password);
-  }
-
   public togglePassword() {
     this.showPassword = !this.showPassword;
-    if (this.passwordToggleIcon == 'eye') {
-      this.passwordToggleIcon = 'eye-off';
+    if (this.passwordToggleIcon == 'visibility') {
+      this.passwordToggleIcon = 'visibility_off';
     }
     else {
-      this.passwordToggleIcon = 'eye';
+      this.passwordToggleIcon = 'visibility';
     }
   }
 
